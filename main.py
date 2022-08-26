ad1c443d936f50d18ec7faf692d4778d3e382313
Marco Fuentes <marcofuentes1607@gmail.com>
2022-08-25 19:22:22 -0600

    🔩 Print symboltable and errors

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pprint
 import sys
 from antlr4 import *
+import antlr4
+from antlr4.tree.Trees import Trees
 from YAPLTree import YAPLTree
 from dist.YAPLLexer import YAPLLexer
 from dist.YAPLParser import YAPLParser
@@ -16,22 +18,24 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     stream.fill()
 
-    print("Tokens:")
-    for token in stream.tokens:
-        print(token)
+    # print("Tokens:")
+    # for token in stream.tokens:
+        # print(token)
 
     parser = YAPLParser(stream)
     parser.removeErrorListeners()
     parser.addErrorListener(customErrorListener())
 
     tree = parser.prog()
-    
     visitor = YAPLTree()
-    visitor.symbolTable()
+    # visitor.symbolTable()
     visitor.visit(tree)
 
     print("\nTabla de simbolos:")
     pprint.pp(visitor.symbolTable)
+
+    print('ERRORS:')
+    print(visitor.errors)
 
 if __name__ == '__main__':
     main(sys.argv)
