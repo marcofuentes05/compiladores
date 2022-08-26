20f72f49494601adbb66d6c430e8a4490f011ac9
Marco Fuentes <marcofuentes1607@gmail.com>
2022-08-25 18:55:00 -0600

    🔩 Create symbol table

--- YAPLTree.py
+++ YAPLTree.py
@@ -1,28 +1,55 @@
 # Generated from YAPL.g4 by ANTLR 4.10.1
+from pydoc import classname
 from dist.YAPLVisitor import YAPLVisitor
+from enum import Enum
 
+class TokenTypes(Enum):
+    CLASS_ID='class-id'
+    VARIABLE_ID='variable-id'
+    OPERATOR = 'operator'
+
+def getOccurrencePosition(element):
+    elementPayload = element.getPayload()
+    return f'{elementPayload.line}:{elementPayload.column}'
 
 # This class defines a complete generic visitor for a parse tree produced by YAPLParser.
 
 class YAPLTree(YAPLVisitor):
     def __init__(self):
-        YAPLVisitor.__init__()
+        YAPLVisitor.__init__(self)
         self.symbolTable = {}
+        self.errors = []
+
 
     # Visit a parse tree produced by YAPLParser#prog.
     def visitProg(self, ctx):
-        for node in ctx.class_prod():
-            child = self.visit(node)
+        # for node in ctx.class_prod():
+        #     child = self.visit(node)
         return self.visitChildren(ctx)
 
 
     # Visit a parse tree produced by YAPLParser#class_prod.
     def visitClass_prod(self, ctx):
+        classNames = [classCtx.getText() for classCtx in ctx.TYPE_ID()]
+        print(classNames)
+        if len(classNames) == 2 and classNames[0] == classNames[1]:
+            self.errors.append(f'Recursive inheritance @ {ctx.TYPE_ID()[0].getPayload().line}')
+        else:
+            for className in ctx.TYPE_ID():
+                if not className.getText() in self.symbolTable:
+                    self.symbolTable[className.getText()] = { 'type': TokenTypes.CLASS_ID.value, 'occurrences': [getOccurrencePosition(className)] }
+            
         return self.visitChildren(ctx)
 
 
     # Visit a parse tree produced by YAPLParser#id.
     def visitId(self, ctx):
+        print(self.symbolTable)
+        id = ctx.OBJECT_ID()
+        if not id.getText() in self.symbolTable:
+            self.symbolTable[id.getText()] = { 'type': TokenTypes.VARIABLE_ID.value, 'occurrences': [getOccurrencePosition(id) ] }
+        else:
+            self.symbolTable[id.getText()]['occurrences'].append(getOccurrencePosition(id))
         return self.visitChildren(ctx)
 
 
@@ -43,8 +70,8 @@ class YAPLTree(YAPLVisitor):
 
     # Visit a parse tree produced by YAPLParser#Add.
     def visitAdd(self, ctx):
-        for node in ctx.expr():
-            print(self.visit(node).get('type') if type(self.visit(node)) is dict else node.getText())
+        # for node in ctx.expr():
+            # print(self.visit(node).get('type') if type(self.visit(node)) is dict else node.getText())
         return self.visitChildren(ctx)
 
 
