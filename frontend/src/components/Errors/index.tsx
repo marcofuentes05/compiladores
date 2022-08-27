a768d043be689b180d0b73aa6f27e2855dc29c60
Marco Fuentes <marcofuentes1607@gmail.com>
2022-08-27 16:17:42 -0600

    🔩 Show if input is valid

--- frontend/src/components/Errors/index.tsx
+++ frontend/src/components/Errors/index.tsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import './styles.css'
 
 interface ErrorsProps {
-    errorsArray: string[]
+    errorsArray: string[]|null;
 }
 
 export const Errors = (props: ErrorsProps) => {
     return (
         <div className="errors">
-            {props.errorsArray.map(
+            {props.errorsArray && props.errorsArray.map(
                 (error, index) => (
                     <p className="error">{index + 1} ~ {error}</p>
                 )
             )}
+            {props.errorsArray && props.errorsArray.length === 0 && <p className="good">Valid input</p>}
         </div>
     )
 }
